272d621e5f39a5b2c4bc59964c308751c2f5a281
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2022-01-09 17:14:21 -0300

    creates a node project

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const { v4: uuidv4 } = require("uuid")
+
+const app = express()
+
+// é um middleware
+app.use(express.json())
+
+const customers = []
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body
+
+    // some() retorna verdadeiro ou falso de acordo com a busca dele
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" })
+    }
+
+    //inserindo dados dentro do array
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return response.status(201).send()
+})
+
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params
+
+    // find() retorna o objeto/dados achados
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    return response.json(customer.statement)
+})
+
+// starta a aplicação
+app.listen(3333) 
